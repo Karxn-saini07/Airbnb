b9304ebb5ca0b1d8780bf0c687de062dbdf70729
Karan <karxnsaini07@gmail.com>
2026-01-18 16:09:31 +0530

    changes in models

--- Models/listing.js
+++ Models/listing.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema= mongoose.Schema;
 
 const listingSchema = new mongoose.Schema({
   title: String,
@@ -20,6 +20,5 @@ const listingSchema = new mongoose.Schema({
     }
   }
 });
-
 
 module.exports = mongoose.model("Listing", listingSchema);
